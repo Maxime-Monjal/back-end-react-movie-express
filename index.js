26df9146f79fd7da61a52e5028bf9db191538afe
Maxime-Monjal <maxime.monjal@gmail.com>
2022-07-01 16:52:28 +0200

    add try and catch with status

--- index.js
+++ index.js
@@ -10,71 +10,119 @@ const API_SECRET = process.env.API_SECRET;
 app.use(cors());
 
 app.get("/", (request, response) => {
-  axios
-    .get(`${BASE_URL}popular?api_key=${API_SECRET}&language=fr&page=1`)
-    .then((res) => {
-      response.send(res.data.results);
-    });
+  try {
+    axios
+      .get(`${BASE_URL}popular?api_key=${API_SECRET}&language=fr&page=1`)
+      .then((res) => {
+        response.status(200);
+        response.send(res.data.results);
+      });
+  } catch (err) {
+    console.error(err.message);
+    response.status(500);
+  }
 });
 
 app.get("/toprated", (request, response) => {
-  axios
-    .get(`${BASE_URL}top_rated?api_key=${API_SECRET}&language=fr&page=1`)
-    .then((res) => {
-      response.send(res.data.results);
-    });
+  try {
+    axios
+      .get(`${BASE_URL}top_rated?api_key=${API_SECRET}&language=fr&page=1`)
+      .then((res) => {
+        response.status(200);
+        response.send(res.data.results);
+      });
+  } catch (err) {
+    console.error(err.message);
+    response.status(500);
+  }
 });
 app.get("/upcoming", (request, response) => {
-  axios
-    .get(`${BASE_URL}upcoming?api_key=${API_SECRET}&language=fr&page=1`)
-    .then((res) => {
-      response.send(res.data.results);
-    });
+  try {
+    axios
+      .get(`${BASE_URL}upcoming?api_key=${API_SECRET}&language=fr&page=1`)
+      .then((res) => {
+        response.status(200);
+        response.send(res.data.results);
+      });
+  } catch (err) {
+    console.error(err.message);
+    response.status(500);
+  }
 });
 app.get("/actors/:id", function (request, response) {
-  axios
-    .get(
-      `${BASE_URL}${request.params.id}/credits?api_key=${API_SECRET}&language=fr`
-    )
-    .then((res) => {
-      response.send(res.data.cast);
-    });
+  try {
+    axios
+      .get(
+        `${BASE_URL}${request.params.id}/credits?api_key=${API_SECRET}&language=fr`
+      )
+      .then((res) => {
+        response.status(200);
+        response.send(res.data.cast);
+      });
+  } catch (err) {
+    console.error(err.message);
+    response.status(500);
+  }
 });
 
 app.get("/movie/similar/:id", function (request, response) {
-  axios
-    .get(
-      `${BASE_URL}${request.params.id}/similar?api_key=${API_SECRET}&language=fr`
-    )
-    .then((res) => {
-      response.send(res.data.results);
-    });
+  try {
+    axios
+      .get(
+        `${BASE_URL}${request.params.id}/similar?api_key=${API_SECRET}&language=fr`
+      )
+      .then((res) => {
+        response.status(200);
+        response.send(res.data.results);
+      });
+  } catch (err) {
+    console.error(err.message);
+    response.status(500);
+  }
 });
 app.get("/movie/:id", function (request, response) {
-  axios
-    .get(`${BASE_URL}${request.params.id}?api_key=${API_SECRET}&language=fr`)
-    .then((res) => {
-      response.send(res.data);
-    });
+  try {
+    axios
+      .get(`${BASE_URL}${request.params.id}?api_key=${API_SECRET}&language=fr`)
+      .then((res) => {
+        response.status(200);
+        response.send(res.data);
+      });
+  } catch (err) {
+    console.error(err.message);
+    response.status(500);
+  }
 });
 
 app.get("/movie/search/:query", (request, response) => {
-  axios
-    .get(
-      `https://api.themoviedb.org/3/search/movie?api_key=${API_SECRET}&language=fr&query=${request.params.query}`
-    )
-    .then((res) => {
-      response.send(res.data);
-    });
+  try {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_SECRET}&language=fr&query=${request.params.query}`
+      )
+      .then((res) => {
+        response.status(200);
+        response.send(res.data);
+      });
+  } catch (err) {
+    console.error(err.message);
+    response.status(500);
+  }
 });
 app.get("/:page", (request, response) => {
-  axios
-    .get(
-      `${BASE_URL}popular?api_key=${API_SECRET}&language=fr&page=${request.params.page}`
-    )
-    .then((res) => {
-      response.send(res.data.results);
-    });
+  try {
+    axios
+      .get(
+        `${BASE_URL}popular?api_key=${API_SECRET}&language=fr&page=${request.params.page}`
+      )
+      .then((res) => {
+        response.status(200);
+        response.send(res.data.results);
+      });
+  } catch (err) {
+    console.error(err.message);
+    response.status(500);
+  }
 });
 
 app.listen(port, () => {
